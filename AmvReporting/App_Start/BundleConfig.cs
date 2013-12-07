40da9dab2d5abb8c86676aab4335a0c91c65fa1d
trailmax <trailmax1@gmail.com>
2013-12-07 02:40:45 +0000

    Playing with bundles

--- AmvReporting/App_Start/BundleConfig.cs
+++ AmvReporting/App_Start/BundleConfig.cs
@@ -7,27 +7,28 @@ namespace AmvReporting
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include("~/Scripts/jquery-{version}.js"));
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                "~/Scripts/jquery-{version}.js"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
+                "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
+                "~/Scripts/bootstrap.js",
+                "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css",
-                      "~/Content/Datatables-1.9.4/media/css/jquery.*"));
+                "~/Content/bootstrap.css",
+                "~/Content/site.css",
+                "~/Content/Datatables-1.9.4/media/css/jquery.*"));
 
             bundles.Add(new ScriptBundle("~/bundles/js").Include(
-                    "~/Scripts/DataTables-1.9.4/media/js/jquery.dataTables.min.js",
-                    "~/Scripts/flot/jquery.*",
-                    "~/Scripts/flot/excanvas.js"
-                ));
+                "~/Scripts/DataTables-1.9.4/media/js/jquery.dataTables.min.js",
+                "~/Scripts/flot/jquery.*",
+                "~/Scripts/flot/excanvas.js"));
+
 #if !DEBUG
             BundleTable.EnableOptimizations = true;
 #endif
