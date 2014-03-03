6cb6fc0bda52bf5e9a46038a5ee739a41b402a38
trailmax <trailmax1@gmail.com>
2014-03-03 23:17:29 +0000

    Fixed issue where can't modify DB connection string

--- AmvReporting/Domain/DatabaseConnections/Commands/CreateDatabaseDetailsCommand.cs
+++ AmvReporting/Domain/DatabaseConnections/Commands/CreateDatabaseDetailsCommand.cs
@@ -1,11 +1,16 @@
+using System.ComponentModel.DataAnnotations;
 using AmvReporting.Infrastructure.CQRS;
 
 namespace AmvReporting.Domain.DatabaseConnections.Commands
 {
     public class CreateDatabaseDetailsCommand : ICommand
     {
+        [Required]
         public string Name { get; set; }
+
+        [Required]
         public string ConnectionString { get; set; }
+
         public string Description { get; set; }
     }
 }
--- AmvReporting/Domain/DatabaseConnections/Commands/EditDatabaseDetailsCommand.cs
+++ AmvReporting/Domain/DatabaseConnections/Commands/EditDatabaseDetailsCommand.cs
@@ -1,20 +1,10 @@
 using System;
-using System.ComponentModel.DataAnnotations;
 using AmvReporting.Infrastructure.CQRS;
 
 namespace AmvReporting.Domain.DatabaseConnections.Commands
 {
-    public class EditDatabaseDetailsCommand : ICommand
+    public class EditDatabaseDetailsCommand : CreateDatabaseDetailsCommand, ICommand
     {
-        [Required]
         public String Id { get; set; }
-
-        [Required]
-        public String Name { get; set; }
-
-        [Required]
-        public String ConnectionString { get; set; }
-
-        public String Description { get; set; }
     }
 }
