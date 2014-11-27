55bedcca684eb8e128af25922eb3ab68d7ee6e80
Max Vasilyev <max.vasilyev@solab.co.uk>
2014-11-27 17:09:51 +0000

    Sorting the bundles

--- AmvReporting/App_Start/BundleConfig.cs
+++ AmvReporting/App_Start/BundleConfig.cs
@@ -4,27 +4,43 @@ using System.Web.Optimization;
 
 namespace AmvReporting
 {
+    public static class Bundles
+    {
+        public const String ClientJs = "~/bundles/clientjs";
+        public const String ClientCss = "~/bundles/clientcss";
+
+        public const String AdminJs = "~/bundles/adminjs";
+        public const String AdminCss = "~/bundles/admincss";
+
+    }
+
     public class BundleConfig
     {
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                "~/Scripts/jquery-{version}.js",
-                "~/Scripts/jquery-migrate-1.2.1.min.js",
-                "~/Scripts/jquery.validate.js",
-                "~/Scripts/jquery.validate.unobtrusive.js",
-                "~/Scripts/jquery-ui-1.10.3.custom.js"));
+            bundles.Add(new ScriptBundle(Bundles.ClientJs).Include(
+                    "~/Scripts/modernizr-*",
+                    "~/Scripts/jquery-{version}.js",
+                    "~/Scripts/jquery-migrate-1.2.1.min.js",
+                    "~/Scripts/bootstrap.js",
+                    "~/Scripts/respond.js",
+                    "~/Scripts/DataTables/js/jquery.dataTables.js",
+                    "~/Scripts/DataTables/extras/FixedHeader/FixedHeader.js",
+                    "~/Scripts/DataTables/extras/Scroller/js/dataTables.scroller.js",
+                    "~/Scripts/DataTables/extras/TableTools/js/TableTools.js",
+                    "~/Scripts/DataTables/extras/TableTools/js/ZeroClipboard.js",
+                    "~/Scripts/ie8.js",
+                    "~/Scripts/amplify.store.js",
+                    "~/Scripts/jquery.treetable.js",
+                    "~/Scripts/jquery.treetablePersist.js",
+                    "~/Scripts/flot/excanvas.js",
+                    "~/Scripts/flot/jquery.flot.js",
+                    "~/Scripts/flot/jquery.flot.categories.js",
+                    "~/Scripts/flot/jquery.flot.stack.patched.js"
+                ));
 
-            // Use the development version of Modernizr to develop with and learn from. Then, when you're
-            // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                "~/Scripts/modernizr-*"));
 
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                "~/Scripts/bootstrap.js",
-                "~/Scripts/respond.js"));
-
-            var styleBundle = new BetterStyleBundle("~/Content/css").Include(
+            var clientStyleBundle = new BetterStyleBundle(Bundles.ClientCss).Include(
                 "~/Content/bootstrap.css",
                 "~/Content/site.css",
                 "~/Scripts/Datatables/css/jquery.dataTables.css",
@@ -33,37 +49,16 @@ namespace AmvReporting
                 "~/Scripts/Datatables/extras/Scroller/css/dataTables.scroller.css",
                 "~/Scripts/Datatables/extras/TableTools/css/TableTools.css",
                 "~/Scripts/Datatables/extras/TableTools/css/TableTools_JUI.css",
-                "~/Content/toastr.min.css",
                 "~/Content/jquery.treetable.css");
-            styleBundle.Include("~/Content/jquery.treetable.theme.default.css");
-            bundles.Add(styleBundle);
+            clientStyleBundle.Include("~/Content/jquery.treetable.theme.default.css");
+            bundles.Add(clientStyleBundle);
 
-            var mergelyBundle = new BetterStyleBundle("~/Content/mergely_css").Include(
-                "~/Content/mergely.css",
-                "~/Scripts/CodeMirror/codemirror.css",
-                "~/Scripts/CodeMirror/addon/display/fullscreen.css");
-            bundles.Add(mergelyBundle);
-
-
-            bundles.Add(new ScriptBundle("~/bundles/js").Include(
-                "~/Scripts/DataTables/js/jquery.dataTables.js",
-                "~/Scripts/DataTables/extras/FixedHeader/FixedHeader.js",
-                "~/Scripts/DataTables/extras/Scroller/js/dataTables.scroller.js",
-                "~/Scripts/DataTables/extras/TableTools/js/TableTools.js",
-                "~/Scripts/DataTables/extras/TableTools/js/ZeroClipboard.js",
-                "~/Scripts/ie8.js",
-                "~/Scripts/amplify.store.js",
+            bundles.Add(new ScriptBundle(Bundles.AdminJs).Include(
+                "~/Scripts/jquery.validate.js",
+                "~/Scripts/jquery.validate.unobtrusive.js",
+                "~/Scripts/jquery-ui-1.10.3.custom.js",
                 "~/Scripts/toastr.min.js",
-                "~/Scripts/jquery.treetablePersist.js"));
-
-            bundles.Add(new ScriptBundle("~/bundles/flot").Include(
-                "~/Scripts/flot/jquery.flot.js",
-                "~/Scripts/flot/jquery.flot.categories.js",
-                "~/Scripts/flot/jquery.flot.stack.patched.js",
-                "~/Scripts/flot/excanvas.js",
-                "~/Scripts/jquery.treetable.js"));
-
-            bundles.Add(new ScriptBundle("~/bundles/mergely_js").Include(
+                "~/Scripts/searchcursor.js",
                 "~/Scripts/CodeMirror/codemirror.js",
                 "~/Scripts/CodeMirror/mode/sql.js",
                 "~/Scripts/CodeMirror/mode/javascript.js",
@@ -71,8 +66,82 @@ namespace AmvReporting
                 "~/Scripts/CodeMirror/mode/xml.js",
                 "~/Scripts/CodeMirror/mode/htmlmixed.js",
                 "~/Scripts/CodeMirror/addon/display/fullscreen.js",
-                "~/Scripts/mergely.js",
-                "~/Scripts/searchcursor.js"));
+                "~/Scripts/mergely.js"
+                ));
+
+            var adminStyleBundle = new BetterStyleBundle(Bundles.AdminCss).Include(
+                "~/Content/toastr.min.css",
+                "~/Content/mergely.css",
+                "~/Scripts/CodeMirror/codemirror.css",
+                "~/Scripts/CodeMirror/addon/display/fullscreen.css");
+            bundles.Add(adminStyleBundle);
+
+            /*********************************************************************/
+            //bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+            //    "~/Scripts/jquery-{version}.js",
+            //    "~/Scripts/jquery-migrate-1.2.1.min.js",
+            //    "~/Scripts/jquery.validate.js",
+            //    "~/Scripts/jquery.validate.unobtrusive.js",
+            //    "~/Scripts/jquery-ui-1.10.3.custom.js"));
+
+            //// Use the development version of Modernizr to develop with and learn from. Then, when you're
+            //// ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
+            //bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+            //    "~/Scripts/modernizr-*"));
+
+            //bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+            //    "~/Scripts/bootstrap.js",
+            //    "~/Scripts/respond.js"));
+
+            //var styleBundle = new BetterStyleBundle("~/Content/css").Include(
+            //    "~/Content/bootstrap.css",
+            //    "~/Content/site.css",
+            //    "~/Scripts/Datatables/css/jquery.dataTables.css",
+            //    "~/Scripts/Datatables/css/jquery.dataTables_themeroller.css",
+            //    "~/Scripts/Datatables/extras/FixedHeader/FixedHeader.js",
+            //    "~/Scripts/Datatables/extras/Scroller/css/dataTables.scroller.css",
+            //    "~/Scripts/Datatables/extras/TableTools/css/TableTools.css",
+            //    "~/Scripts/Datatables/extras/TableTools/css/TableTools_JUI.css",
+            //    "~/Content/toastr.min.css",
+            //    "~/Content/jquery.treetable.css");
+            //styleBundle.Include("~/Content/jquery.treetable.theme.default.css");
+            //bundles.Add(styleBundle);
+
+            //var mergelyBundle = new BetterStyleBundle("~/Content/mergely_css").Include(
+            //    "~/Content/mergely.css",
+            //    "~/Scripts/CodeMirror/codemirror.css",
+            //    "~/Scripts/CodeMirror/addon/display/fullscreen.css");
+            //bundles.Add(mergelyBundle);
+
+
+            //bundles.Add(new ScriptBundle("~/bundles/js").Include(
+            //    "~/Scripts/DataTables/js/jquery.dataTables.js",
+            //    "~/Scripts/DataTables/extras/FixedHeader/FixedHeader.js",
+            //    "~/Scripts/DataTables/extras/Scroller/js/dataTables.scroller.js",
+            //    "~/Scripts/DataTables/extras/TableTools/js/TableTools.js",
+            //    "~/Scripts/DataTables/extras/TableTools/js/ZeroClipboard.js",
+            //    "~/Scripts/ie8.js",
+            //    "~/Scripts/amplify.store.js",
+            //    "~/Scripts/toastr.min.js",
+            //    "~/Scripts/jquery.treetablePersist.js"));
+
+            //bundles.Add(new ScriptBundle("~/bundles/flot").Include(
+            //    "~/Scripts/flot/jquery.flot.js",
+            //    "~/Scripts/flot/jquery.flot.categories.js",
+            //    "~/Scripts/flot/jquery.flot.stack.patched.js",
+            //    "~/Scripts/flot/excanvas.js",
+            //    "~/Scripts/jquery.treetable.js"));
+
+            //bundles.Add(new ScriptBundle("~/bundles/mergely_js").Include(
+            //    "~/Scripts/CodeMirror/codemirror.js",
+            //    "~/Scripts/CodeMirror/mode/sql.js",
+            //    "~/Scripts/CodeMirror/mode/javascript.js",
+            //    "~/Scripts/CodeMirror/mode/css.js",
+            //    "~/Scripts/CodeMirror/mode/xml.js",
+            //    "~/Scripts/CodeMirror/mode/htmlmixed.js",
+            //    "~/Scripts/CodeMirror/addon/display/fullscreen.js",
+            //    "~/Scripts/mergely.js",
+            //    "~/Scripts/searchcursor.js"));
 
 
 #if !DEBUG
